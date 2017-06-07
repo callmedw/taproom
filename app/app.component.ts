8296af0c18bd881f79a71f98f800410a1445ef92
Dana Weiss and Susha Dore <susha@>
2017-06-07 14:08:14 -0700

    initialize files

---
+++ app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { Keg } from './keg.model';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <div class="container">
+      <h1>Tap Room</h1>
+    </div>
+  `
+})
+
+export class AppComponent {
+
+}
